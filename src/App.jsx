caa179e63e26835c775885942592535592a78485
yahya-mohamoud <yahyamohamoud625@gmail.com>
2025-11-05 06:22:48 +0300

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         setHistory(prevHistory => [...prevHistory, quote]);
       }
 
-      const res = await fetch('https://api.quotable.io/random');
+      const res = await fetch('http://api.quotable.io/random');
       const data = await res.json();
       setQuote(data);
     } catch (error) {
